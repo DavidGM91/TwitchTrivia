3f4d5317feb86fc476c8239f5f593ccee05aad58
David Gonzalez Marchante <david.gonzalez.marchante@est.fib.upc.edu>
2020-02-03 17:51:13 +0100

    Added configuration helper instead of Template

--- bot.js
+++ bot.js
@@ -1,25 +1,67 @@
 const tmi = require('tmi.js');
-const config = require('./config.json');
 const trivia = require('./Trivia.js');
-const preguntas = require('./questions.json');
+const fs = require('fs');
+const readline = require('readline');
+var config;
+var client;
 
-// Define configuration options
-const opts = {
-  identity: {
-    username: config.username,
-    password: config.token
-  },
-  channels: config.channels
-};
-// Create a client with our options
-const client = new tmi.client(opts);
+if(!fs.existsSync('./config.json'))
+{
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  var _config = new Object();
+  console.log("No configuration file found, please complete the following to create yours.");
+  rl.question("The username of your bot: ", function(res){
+    _config.username = res;
+    rl.question("The oAuth token of your bot: ", function(res){
+      _config.token = res;
+      rl.question("The channels where you want your bot to work separated by spaces if more than one: ", function(res){
+        _config.channels = res.split(" ");
+        rl.question("The command to request a question(starting with !): ", function(res){
+          _config.questionComand = res;
+          rl.question("The command to answer a question(starting with !): ", function(res){
+            _config.answerComand = res;
+            console.log("Congratulations you've completed the configuration of your bot");
+            config = _config;
+            fs.writeFile('config.json',JSON.stringify(_config,null,2), function (err){
+              console.log('Saved new configuration file.');
+            });
+            client = connect_to_Twitch();
+          });
+        });
+      });
+    });
+  });
+}
+else
+{
+  config = require('./config.json') ;
+  client = connect_to_Twitch();
+}
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+function connect_to_Twitch()
+{
+  // Define configuration options
+  const opts = {
+    identity: {
+      username: config.username,
+      password: config.token
+    },
+    channels: config.channels
+  };
+  // Create a client with our options
+  const client = new tmi.client(opts);
 
-// Connect to Twitch:
-client.connect();
+  // Register our event handlers (defined below)
+  client.on('message', onMessageHandler);
+  client.on('connected', onConnectedHandler);
+
+  // Connect to Twitch:
+  client.connect();
+  return client;
+}
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msgraw, self) {
@@ -32,13 +74,13 @@ function onMessageHandler (target, context, msgraw, self) {
         const msg = msgraw.split(" ");
         const user = context['display-name'];
         // If the command is known, let's execute it
-        if (msg[0] === preguntas.questionComand)
+        if (msg[0] === config.questionComand)
         {
            trivia.nueva(user, function (texto){
                 client.say(target, texto );
             });    
         }
-        else if (msg[0] === preguntas.answerComand)
+        else if (msg[0] === config.answerComand)
         {
             trivia.responde(user, msg[1], function (texto){
                 client.say(target, texto );
